756332b068e379944bdfa3b7442c1c47bb55b913
lizhaowen <lizhaowen0722@gmail.com>
2026-08-05 04:46:04 +0800

    chore(release): bump version to 0.2.1 (TAG-01)

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hawkeyexb.ppass"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
